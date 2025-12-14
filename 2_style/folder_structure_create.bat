5674f8b186596e67d82c7d4084772512952f00ec
Harry Shaper <harryshaper@gmail.com>
2025-12-14 16:29:06 +0000

    folder structure maker

---
+++ 2_style/folder_structure_create.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:::Makes a VFX Folder Structure
+
+::Assets/Vehicles
+md D:\VFX\assets_and_courses\courses\Advanced_python_course\2_Style\python_scripts\Assets\Vehicles\Geo
+md D:\VFX\assets_and_courses\courses\Advanced_python_course\2_Style\python_scripts\Assets\Vehicles\Texture
+
+::Assets/Env
+md D:\VFX\assets_and_courses\courses\Advanced_python_course\2_Style\python_scripts\Assets\Environments\Geo
+md D:\VFX\assets_and_courses\courses\Advanced_python_course\2_Style\python_scripts\Assets\Environments\Texture
+
+::Assets/Props
+md D:\VFX\assets_and_courses\courses\Advanced_python_course\2_Style\python_scripts\Assets\Props\Geo
+md D:\VFX\assets_and_courses\courses\Advanced_python_course\2_Style\python_scripts\Assets\Props\Texture
+
+
+exit
