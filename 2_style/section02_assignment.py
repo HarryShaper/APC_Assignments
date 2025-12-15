e86851e95f7f2875f0a25e9e1bda0d4a36b0e608
Harry Shaper <harryshaper@gmail.com>
2025-12-15 11:26:15 +0000

    upload assignment

---
+++ 2_style/section02_assignment.py
@@ -0,0 +1,11 @@
+'''*************************************************
+content      Section 2: Style
+			 Assignment for section 2
+
+version      0.0.1
+date   		 15-12-2025
+
+author       Harry Shaper <harryshaper@gmail.com>
+
+*************************************************'''
+
